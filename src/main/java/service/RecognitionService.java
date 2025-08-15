cdcda85ff9e3910dc94d839a344d5e3c58762b9d
Artem Serebryakov <sertem96@gmail.com>
2025-08-16 02:10:33 +0300

    Refactor suit detection

--- src/main/java/service/RecognitionService.java
+++ src/main/java/service/RecognitionService.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @Data
 public class RecognitionService {
@@ -18,6 +19,7 @@ public class RecognitionService {
     int[][] positions = {{0,86}, {55, 0}, {55,86}};
     int[][] leftTops = {{147, 586}, {219, 586}, {290, 586}, {361, 586}, {432, 586}};
     int[][] suits = {{37, 63}, {48, 56}, {45, 58}};
+    private Map<String, String> suitsMap = Map.of("11", "heartsСердца", "10", "diamondsБуби", "01", "clubsТрефы", "00", "spadesПики");
 
     public void doRecognition(BufferedImage table) throws IOException {
 
@@ -30,7 +32,7 @@ public class RecognitionService {
             Color color = new Color(cardImg.getRGB(1, 3));
             if (color.getBlue() > 100 && color.getGreen() > 100 && color.getRed() > 100) {
                 String suit = detectCardSuit(cardImg);
-                File cardFile = new File("tmp\\" + "table = " + cnt + "card = " + n++ + suit + ".png");
+                File cardFile = new File("tmp\\" + "table = " + cnt + "; card = " + n++ + suit + ".png");
                 ImageIO.write(cardImg, "png", cardFile);
                 fileName.append(suit);
             }
@@ -46,17 +48,13 @@ public class RecognitionService {
         Color color1 = new Color(card.getRGB(suits[0][0], suits[0][1]));
         Color color2 = new Color(card.getRGB(suits[1][0], suits[1][1]));
         Color color3 = new Color(card.getRGB(suits[2][0], suits[2][1]));
-        if (color1.getRed() > color1.getGreen()) {
-            if (color2.getRed() > color2.getGreen()) {
-                return "heartsСердца";
-            } else {
-                return "diamondsБуби";
-            }
-        } else if (color3.getRed() > 100) {
-            return "clubsТрефы";
+        String result = color1.getRed() > color1.getGreen() ? "1" : "0";
+        if (result.equals("1")) {
+            result += color2.getRed() > color2.getGreen() ? "1" : "0";
         } else {
-            return "spadesПики";
+            result += color3.getRed() > 100 ? "1" : "0";
         }
+        return suitsMap.get(result);
     }
 
     private String detectCardRang(BufferedImage card) {
